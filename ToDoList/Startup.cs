fadc3e0e3b9990f4214e873e33dfbd2c36dd789e
Shinjice <Tdna84@gmail.com>
2020-11-09 15:28:01 +0100

    09 Nov 2020 15:28:01

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -67,7 +67,7 @@ namespace ToDoList
             
             app.Use(async (context, next) =>
             {
-                context.Response.Headers.Add("X-Frame-Options", "ALLOW-FROM https://todolisttam.azurewebsites.com/");
+                context.Response.Headers.Add("X-Frame-Options", "ALLOW");
                 await next();
             });
 
